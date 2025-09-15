18e204dac3169b68f34470702c0fd85ee1ca8ebe
Venu-16 <venumadhav2212@gmail.com>
2025-09-15 16:52:58 +0530

    app.py modified

--- Backend/app.py
+++ Backend/app.py
@@ -1,25 +1,28 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-import joblib
+import pickle
 import os
 
-app= Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 
-model = joblib.load("Fake-Job-Postings-Detection-/models/xgb_final_model.joblib")
-vectorizer = joblib.load("Fake-Job-Postings-Detection-/models/tfidf_vectorizer.joblib")
+# Load pickle files instead of joblib
+with open("models/xgb_final_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+with open("models/tfidf_vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    text = data.get("message","");
+    text = data.get("message", "")
 
+    # Transform text and predict
     features = vectorizer.transform([text])
     prediction = model.predict(features)[0]
 
-    return jsonify({"prediction :", str(prediction)})
+    return jsonify({"prediction": str(prediction)})
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
