f1168754ef4bc6ae01191eeb664f61b2823e6f4a
Venu-16 <venumadhav2212@gmail.com>
2025-09-18 23:27:41 +0530

    images are added into frontend to display.

--- frontend/frontend/src/Results.js
+++ frontend/frontend/src/Results.js
@@ -1,24 +1,47 @@
 import React from "react";
-import "./JobPredictor.css";git
+import "./results.css";
+import accuracyImg from "./MetricImages/model_comparison_accuracy.png";
+import Precision from "./MetricImages/model_comparison_precision.png";
+import Recall from "./MetricImages/model_comparison_recall.png";
+import F1 from "./MetricImages/model_comparison_f1_score.png";
+import confusionMatrix from "./MetricImages/confusion_matrix.png";
+import limeImg from "./MetricImages/lime_output.png";
 
 function Results() {
   return (
     <div className="results-container">
       <h2 className="results-title">📊 Model Performance Results</h2>
 
-      <div className="results-section">
-        <h3>Accuracy Comparison</h3>
-        <img src="/accuracy.png" alt="Accuracy Comparison" className="results-img" />
-      </div>
+      <div className="results-grid">
+        <div className="results-card">
+          <h3>Accuracy Comparison</h3>
+          <img src={accuracyImg} alt="Accuracy Comparison" className="results-img" />
+        </div>
 
-      <div className="results-section">
-        <h3>Confusion Matrix</h3>
-        <img src="/confusion.png" alt="Confusion Matrix" className="results-img" />
-      </div>
+        <div className="results-card">
+          <h3>Confusion Matrix</h3>
+          <img src={confusionMatrix} alt="Confusion Matrix" className="results-img" />
+        </div>
 
-      <div className="results-section">
-        <h3>Precision / Recall / F1 Score</h3>
-        <img src="/f1.png" alt="F1 Score Comparison" className="results-img" />
+        <div className="results-card">
+          <h3>Precision</h3>
+          <img src={Precision} alt="Precision Comparison" className="results-img" />
+        </div>
+
+        <div className="results-card">
+          <h3>Recall</h3>
+          <img src={Recall} alt="Recall Comparison" className="results-img" />
+        </div>
+
+        <div className="results-card">
+          <h3>F1 Score Comparison</h3>
+          <img src={F1} alt="F1 Score Comparison" className="results-img" />
+        </div>
+        <div className="results-card">
+        <h3>LIME Explanation Example</h3>
+        <img src={limeImg} alt="LIME Explanation" className="results-img" />
+        </div>
+
       </div>
     </div>
   );
